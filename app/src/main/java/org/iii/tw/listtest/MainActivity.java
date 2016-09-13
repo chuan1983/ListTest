05ace06c5254fd5059bc36cc03851d6be54be49b
chuan1983 <chuan1983126@gmail.com>
2016-09-13 14:41:05 +0800

    v4

--- app/src/main/java/org/iii/tw/listtest/MainActivity.java
+++ app/src/main/java/org/iii/tw/listtest/MainActivity.java
@@ -2,12 +2,17 @@ package org.iii.tw.listtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private ListView list;
@@ -34,6 +39,21 @@ public class MainActivity extends AppCompatActivity {
         adapter = new SimpleAdapter(
                         this,data,R.layout.layout_item ,from,to);
         list.setAdapter(adapter);
+
+//        list.setOnClickListener(new View.OnClickListener() {      //錯誤寫法
+//            @Override
+//            public void onClick(View v) {
+//                Log.d("brad","ok");
+//            }
+//        });
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long 1) {
+                Log.d("brad","i=" + i);
+                Toast.makeText(MainActivity.this, "ok", Toast.LENGTH_SHORT).show();   //這裡的this 代表AdapterView而不是context  ,Toast.LENGTH_SHORT顯示ok的時間
+            }
+        });
     }
     public void addItem(){
         String input = inputTitle.getText().toString();
